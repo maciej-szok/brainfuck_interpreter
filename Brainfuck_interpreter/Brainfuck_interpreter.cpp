87b86339a8efe44544735744f37f3a0b865a59f1
macianga <macieq2048@gmail.com>
2021-05-27 18:23:59 +0200

    final version - for now

--- Brainfuck_interpreter/Brainfuck_interpreter.cpp
+++ Brainfuck_interpreter/Brainfuck_interpreter.cpp
@@ -6,19 +6,7 @@
 
 int main(int argc, char* argv[])
 {
-    int counter;
     
-    //printf("Program Name Is: %s", argv[0]);
-    //if (argc == 1)
-    //    printf("\nNo Extra Command Line Argument Passed Other Than Program Name");
-    //if (argc >= 2)
-    //{
-    //    printf("\nNumber Of Arguments Passed: %d", argc);
-    //    printf("\n----Following Are The Command Line Arguments Passed----");
-    //    for (counter = 0; counter < argc; counter++)
-    //        printf("\nargv[%d]: %s", counter, argv[counter]);
-    //}
-    //return 0;
     std::string command = "";
 
     std::string line;
@@ -43,8 +31,6 @@ int main(int argc, char* argv[])
     in.setProgramm(command);
 
     in.interpret();
-
-//    std::cin.get();
 
 }
 
--- Brainfuck_interpreter/Interpreter.cpp
+++ Brainfuck_interpreter/Interpreter.cpp
@@ -3,6 +3,7 @@
 #include <algorithm> 
 #include "CONFIG.h"
 
+
 Interpreter::Interpreter()
 {
 	memoryPointer = 0;
@@ -17,6 +18,7 @@ void Interpreter::output()
 	std::cout << (char)memory[memoryPointer];
 }
 
+
 void Interpreter::incrementMemoryPointer()
 {
 	memoryPointer++;
@@ -24,12 +26,16 @@ void Interpreter::incrementMemoryPointer()
 		memory.push_back(0);
 
 	//TODO implement exception if not enough memory
+	throw "Not enough memory.";
 }
+
 
 void Interpreter::decrementMemoryPointer()
 {
 	memoryPointer--;
 	//TODO implement exception if memoryPointer is smaller then 0
+	throw "Memory pointer could not point to memory cell with index smaller then 0";
+
 }
 
 void Interpreter::incrementMemory()
@@ -121,6 +127,7 @@ void Interpreter::interpret()
 		char currentCommand = this->getCurrentCommand();
 
 		if (currentCommand == '\0') running = false;
+		//skip a char if it isnt specified as a command
 		if (!this->isValidInstruction(currentCommand)) continue;
 		
 		if (currentCommand == BRAINFUCK_OUTPUT) this->output();
